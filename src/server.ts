dc600d42addd8012e0a36a6a081f50abfdf6aa7c
Douglas Held Pacito <analistasistemas2.ti@comercialmariano.com.br>
2020-08-22 17:47:55 -0400

    Eslint e Jest

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
-import '@controllers/UsersController'
+import '@controllers/UsersController';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ mensagem: 'Hello World' });
-});
+app.get('/', (request, response) => response.json({ mensagem: 'Hello World' }));
 
 app.listen(3333, () => {
-    console.log("🚀 Server started on http://localhost:3333");
+    console.log('🚀 Server started on http://localhost:3333');
 });
